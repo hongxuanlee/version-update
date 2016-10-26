adb9666947e9569433f3d4d19de586c786bae203
kinolee <lihongxuan213@gmail.com>
2016-10-26 23:53:23 +0800

    one module update

--- test/test_update.js
+++ test/test_update.js
@@ -1,8 +1,17 @@
 const expect = require('chai').expect;
 const fs = require('mz/fs');
-describe('version update', () => {
-  it('should update version', () => {
-    
-  })
+const path = require('path');
+const npmUpdate = require('../index.js');
 
+describe('version update', function() {
+  it('should update version', function(done) {
+    var p = path.join(__dirname, 'feature/test1');
+    console.log(p);
+    npmUpdate(p, 'lodash', '4.0.0').then((data) => {
+      console.log('done', data); 
+      done();
+    }).catch(function(e){
+      console.log('error', e);
+    });
+  })
 });
